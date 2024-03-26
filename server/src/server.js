d21cf5de55224b4c31706cd3efdbad3014252a10
Nicolas Thompson <mail@nicolasthompson.com>
2024-03-26 15:26:54 +0000

    Adds https to server

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
-const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
 
 const app = require('./app');
 
@@ -7,11 +9,17 @@ const { postgresConnection } = require('./services/postgres');
 
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer(app);
+const server = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+  },
+  app
+);
 
 async function startServer() {
   await postgresConnection();
-  await loadProducts();
+  // await loadProducts();
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
