6ea80efa5c54127872afcfede62a4c4a99fba0ad
Nicolas Thompson <mail@nicolasthompson.com>
2024-03-26 11:19:09 +0000

    Logs request with Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,18 @@
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
+var rfs = require('rotating-file-stream');
 
 const productsRouter = require('./routes/products/products.router');
 
 const app = express();
+
+var accessLogStream = rfs.createStream('access.log', {
+  interval: '1d',
+  path: path.join(__dirname, 'log'),
+});
+
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
