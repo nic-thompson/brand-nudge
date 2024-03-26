db0967fd2297deae82abc29479e89d837dc4cf7c
Nicolas Thompson <mail@nicolasthompson.com>
2024-03-26 11:02:20 +0000

    Serves React.js Front End in Production

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const productsRouter = require('./routes/products/products.router');
@@ -5,7 +6,12 @@ const productsRouter = require('./routes/products/products.router');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/products', productsRouter);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
