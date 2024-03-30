065f1c7f1d400fd60a79921df70fd2d51528e869
Nicolas Thompson <mail@nicolasthompson.com>
2024-03-30 14:46:04 +0000

    Set up postgres connection beforeAll Jest tests

--- server/src/routes/products/products.test.js
+++ server/src/routes/products/products.test.js
@@ -1,11 +1,17 @@
 const request = require('supertest');
 const app = require('../../app');
 
-describe('Test GET /products', () => {
-  test('It should respond with 200 success', async () => {
-    const response = await request(app)
-      .get('/products')
-      .expect('Content-Type', /json/)
-      .expect(200);
+describe('Products API', () => {
+  beforeAll(async () => {
+    await require('../../services/postgres').postgresConnection();
+    await require('../../services/postgres').sequelize.sync({ force: true });
+  });
+  describe('Test GET /products', () => {
+    test('It should respond with 200 success', async () => {
+      const response = await request(app)
+        .get('/products')
+        .expect('Content-Type', /json/)
+        .expect(200);
+    });
   });
 });
