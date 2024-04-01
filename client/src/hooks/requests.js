831f182fb0c281df9461eb3df3415bca352f6bad
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-01 17:43:51 +0100

    Removes unreachable code

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -4,28 +4,6 @@ async function httpGetProducts() {
   const response = await fetch(`${API_URL}/products`);
   const fetchedProducts = await response.json();
   return fetchedProducts.sort((a, b) => a.retailer - b.retailer);
-  const products = [
-    {
-      id: 1,
-      name: 'Product 1',
-      description: 'This is the description for Product 1.',
-      price: 10.99,
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      description: 'This is the description for Product 2.',
-      price: 19.99,
-    },
-    {
-      id: 3,
-      name: 'Product 3',
-      description: 'This is the description for Product 3.',
-      price: 7.49,
-    },
-  ];
-
-  return products;
 }
 
 export { httpGetProducts };
