de7a65a26fda1074d1f479b4100205b7387ec506
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-02 18:07:48 +0100

    Adds sorting and pagination to products table

--- client/src/componets/Table.js
+++ client/src/componets/Table.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTable, useSortBy } from 'react-table';
+import { useTable, useSortBy, usePagination } from 'react-table';
 
 const Table = (props) => {
   const { products } = props;
@@ -25,23 +25,83 @@ const Table = (props) => {
     [products]
   );
 
-  const { getTableProps, headerGroups, rows, prepareRow } = useTable(
+  const {
+    getTableProps,
+    headerGroups,
+    page,
+    prepareRow,
+    state: { pageIndex, pageSize },
+    gotoPage,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageCount,
+    setPageSize,
+  } = useTable(
     {
       columns,
       data,
+      initialState: { pageIndex: 0, pageSize: 10 },
     },
-    useSortBy // Add useSortBy hook here
+    useSortBy,
+    usePagination
   );
 
   return (
     <div className="container">
+      <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={previousPage} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={nextPage} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageCount}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: '50px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
       <table {...getTableProps()}>
         <thead>
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
                 <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {/* Render column header with sorting icon */}
                   {column.render('Header')}
                   <span>
                     {column.isSorted
@@ -56,7 +116,7 @@ const Table = (props) => {
           ))}
         </thead>
         <tbody>
-          {rows.map((row) => {
+          {page.map((row, i) => {
             prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
