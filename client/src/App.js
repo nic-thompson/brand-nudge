657b076bab69af47c605d8012478db593e809c2c
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-02 13:54:03 +0100

    Removes unused logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import AppLayout from './pages/AppLayout';
-import logo from './brand_nudge_logo.jpg';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -4,12 +4,11 @@ const Products = (props) => {
   const { products } = props;
 
   if (!products.length) {
-    return <p>Loading...</p>;
+    return <>Loading...</>;
   }
 
   return (
     <div>
-      <h1>Products</h1>
       <Table products={products} />
     </div>
   );
